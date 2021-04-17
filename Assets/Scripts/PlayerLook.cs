f6c7282e14ae6f0e1622ceaffaecef2c97ec5948
kostabuntsev <kosta.buntsev@gmail.com>
2021-04-17 13:09:51 +1000

    Worked on the terrain and player camera

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -16,7 +16,7 @@ using UnityEngine;
     // Start is called before the first frame update
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void HandleMovement()
@@ -31,6 +31,11 @@ using UnityEngine;
 
             cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
             playerBody.Rotate(Vector3.up * mouseX);
+
+            if(Input.GetButtonDown("Cancel"))
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
         }
     }
 
